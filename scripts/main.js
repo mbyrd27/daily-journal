81378fa8e1e55114feda62828ac7b32df9004cdb
Matt Byrd <mbyrd7886@outlook.com>
2020-12-30 17:47:38 -0500

    added journal form component

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
 import { getEntries } from './JournalDataProvider.js';
 import { EntryListComponent } from './JournalEntryList.js'
+import { JournalForm } from './JournalForm.js';
 
 EntryListComponent();
 
-getEntries();
+//getEntries();
+
+JournalForm();
