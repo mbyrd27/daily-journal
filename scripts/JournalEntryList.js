eaf0ce94476d8207084daa195121de78b3438b81
Matt Byrd <mbyrd7886@outlook.com>
2021-01-10 16:42:10 -0500

    added functionality to delete buttons

--- scripts/JournalEntryList.js
+++ scripts/JournalEntryList.js
@@ -1,4 +1,4 @@
-import { getEntries, useJournalEntries } from './JournalDataProvider.js'
+import { deleteEntry, getEntries, useJournalEntries } from './JournalDataProvider.js'
 import { journalEntry } from './JournalEntry.js'
 
 const entryLog = document.querySelector('.entries');
@@ -14,3 +14,15 @@ const EntryListComponent = () => {
 eventHub.addEventListener('entryAdded', EntryListComponent)
 
 EntryListComponent();
+
+eventHub.addEventListener('click', clickEvent => {
+    if (clickEvent.target.id.startsWith('deleteEntry--')) {
+        const [prefix, id] = clickEvent.target.id.split('--')
+        console.log(id)
+        deleteEntry(id)
+            .then(() => {
+                const entries = useJournalEntries();
+                entryLog.innerHTML = entries.map(entry => journalEntry(entry))
+            })
+    }
+})
