41917550b0d26d52526152c650f6dc5215f15728
Matt Byrd <mbyrd7886@outlook.com>
2021-02-02 22:54:44 -0500

    tags now appear on journal entries if added

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -1,11 +1,13 @@
-export const journalEntry = (entry) => {
+export const journalEntry = (entry, tags) => {
     return`
-    <li class="entry">
+    <li class="entry" id="entry--${entry.id}">
         <p class="entry-date">${entry.date}</p>
         <p class="entry-concepts"><span class="heading">Concept(s): </span>${entry.concept}</p>
         <p class="entry-mood"><span class="heading">Today's Mood: </span>${entry.mood.label}</p>
-        <p class="entry-content">${entry.entry}
-        </p>
+        <p class="entry-content">${entry.entry}</p> 
+        <p class="entry-tags"><span class="heading">Tags: </span>
+        ${tags.map(tag => `<em> #${tag.subject}</em>`)}
+        </p> 
         <div class="btn-holder">
             <button class="entry-btn edit">Edit</button>
             <button id="deleteEntry--${entry.id}"class="entry-btn delete">Delete</button>
@@ -13,3 +15,4 @@ export const journalEntry = (entry) => {
     </li>
     `
 }
+
